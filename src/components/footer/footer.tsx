67f53178bb1ad4931d8e8666475a2a6e357e8bd0
RonaldGuilhermePDS <RonaldGuilhermePDS@gmail.com>
2023-04-17 12:52:50 -0300

    feat: change footer labels

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -16,10 +16,10 @@ const Footer: FC = () => {
           <Grid item xs={12} md={5}>
             <Box sx={{ width: { xs: '100%', md: 360 }, mb: { xs: 3, md: 0 } }}>
               <Typography component="h2" variant="h2" sx={{ mb: 2 }}>
-                Coursespace
+                Consult ST Engenharia
               </Typography>
               <Typography variant="subtitle1" sx={{ letterSpacing: 1, mb: 2 }}>
-                Coursespace is an online learning platform that has been operating since 2018 until now.
+                A Consult ST Engenharia está em operação desde 2001, entregando o seu melhor para satisfazer seus clientes.
               </Typography>
               <FooterSocialLinks />
             </Box>
