f0295aec40efc9f5e1d7a65857d5db4305412d14
RonaldGuilhermePDS <RonaldGuilhermePDS@gmail.com>
2023-04-17 12:57:09 -0300

    feat: remove auth navigation

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -4,7 +4,7 @@ import Container from '@mui/material/Container'
 import IconButton from '@mui/material/IconButton'
 import useMediaQuery from '@mui/material/useMediaQuery'
 import { Logo } from '@/components/logo'
-import { Navigation, AuthNavigation } from '@/components/navigation'
+import { Navigation } from '@/components/navigation'
 import { useTheme } from '@mui/material/styles'
 import { Menu, Close } from '@mui/icons-material'
 
@@ -45,7 +45,6 @@ const Header: FC = () => {
           >
             <Box /> {/* Magic space */}
             <Navigation />
-            <AuthNavigation />
             {visibleMenu && matchMobileView && (
               <IconButton
                 sx={{
