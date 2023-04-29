8b3b6e118ae5d8eb02f1474eca48b0a2828f805f
RonaldGuilhermePDS <RonaldGuilhermePDS@gmail.com>
2023-04-29 19:17:17 -0300

    feat: remove course menu

--- src/components/footer/footer-navigation.tsx
+++ src/components/footer/footer-navigation.tsx
@@ -6,25 +6,6 @@ import type { Navigation } from '@/interfaces/navigation'
 import { navigations as headerNavigations } from '@/components/navigation/navigation.data'
 import { FooterSectionTitle } from '@/components/footer'
 
-const courseMenu: Array<Navigation> = [
-  {
-    label: 'UI/UX Design',
-    path: '#',
-  },
-  {
-    label: 'Mobile Development',
-    path: '#',
-  },
-  {
-    label: 'Machine Learning',
-    path: '#',
-  },
-  {
-    label: 'Web Development',
-    path: '#',
-  },
-]
-
 const pageMenu = headerNavigations
 
 const companyMenu: Array<Navigation> = [
@@ -41,7 +22,7 @@ interface NavigationItemProps {
 
 const NavigationItem: FC<NavigationItemProps> = ({ label, path }) => {
   return (
-    <Link href={path} passHref>
+    <Link  href={path} passHref>
       <MuiLink
         underline="hover"
         sx={{
@@ -59,13 +40,7 @@ const NavigationItem: FC<NavigationItemProps> = ({ label, path }) => {
 const FooterNavigation: FC = () => {
   return (
     <Grid container spacing={2}>
-      <Grid item xs={12} md={4}>
-        <FooterSectionTitle title="Course" />
-        {courseMenu.map(({ label, path }, index) => (
-          <NavigationItem key={index + path} label={label} path={/* path */ '#'} />
-        ))}
-      </Grid>
-      <Grid item xs={12} md={4}>
+      <Grid item xs={12} md={6}>
         <FooterSectionTitle title="Menu" />
         {pageMenu.map(({ label, path }, index) => (
           <NavigationItem key={index + path} label={label} path={path} />
